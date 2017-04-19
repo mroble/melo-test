12d9bda7685557e2f4e732526880ccb2478b7fc0
Maureen Roble <moroble@gmail.com>
2017-04-19 15:21:49 -0400

    add script for water animation

--- js/states/stateTitle.js
+++ js/states/stateTitle.js
@@ -6,16 +6,68 @@ var StateTitle = {
     }
     , create: function () {
 
-        var titleText = game.add.text(game.width / 2, game.height / 2 - 100, "Melo The Umang Boy");
+        var fragmentSrc = [
+
+        "precision mediump float;",
+
+        "uniform float     time;",
+        "uniform vec2      resolution;",
+        "uniform vec2      mouse;",
+
+        "#define MAX_ITER 4",
+
+        "void main( void )",
+        "{",
+            "vec2 v_texCoord = gl_FragCoord.xy / resolution;",
+
+            "vec2 p =  v_texCoord * 8.0 - vec2(20.0);",
+            "vec2 i = p;",
+            "float c = 1.0;",
+            "float inten = .05;",
+
+            "for (int n = 0; n < MAX_ITER; n++)",
+            "{",
+                "float t = time * (1.0 - (3.0 / float(n+1)));",
+
+                "i = p + vec2(cos(t - i.x) + sin(t + i.y),",
+                "sin(t - i.y) + cos(t + i.x));",
+
+                "c += 1.0/length(vec2(p.x / (sin(i.x+t)/inten),",
+                "p.y / (cos(i.y+t)/inten)));",
+            "}",
+
+            "c /= float(MAX_ITER);",
+            "c = 1.5 - sqrt(c);",
+
+            "vec4 texColor = vec4(0.0, 0.01, 0.015, 1.0);",
+
+            "texColor.rgb *= (1.0 / (1.0 - (c + 0.05)));",
+
+            "gl_FragColor = texColor;",
+        "}"
+    ];
+
+    filter = new Phaser.Filter(game, null, fragmentSrc);
+    filter.setResolution(800, 600);
+
+    sprite = game.add.sprite();
+    sprite.width = 800;
+    sprite.height = 600;
+
+    sprite.filters = [ filter ];
+
+     var titleText = game.add.text(game.width / 2, game.height / 2 - 100, "Melo The Umang Boy");
         titleText.fill = "#ffffff";
         titleText.anchor.set(0.5, 0.5);
 
         this.btnStart = gameButtons.addButton("start", -1, -1, this.startGame, this);
+
     }
     , startGame: function () {
         game.state.start("StateMain");
     }
     , update: function () {
+        filter.update(game.input.activePointer);
 
     }
 
