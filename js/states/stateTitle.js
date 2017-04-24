b8e6a57087cce6606bbeacf6085aaf2b5741ac16
Maureen Roble <moroble@gmail.com>
2017-04-23 20:22:46 -0400

    correct error

--- js/states/stateTitle.js
+++ js/states/stateTitle.js
@@ -6,9 +6,6 @@ var StateTitle = {
     }
     , create: function () {
 
-        overText.anchor.set(0.5, 0.5);
-
-
         this.title=game.add.sprite(game.world.centerX-200,game.world.centerY-175, "title");
 
         this.btnPlayAgain = gameButtons.addButton("playAgain", -1, -1, this.playAgain, this);
