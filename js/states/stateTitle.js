f88cf49885c93646ce8da31b5492a5e9dc621613
Maureen Roble <moroble@gmail.com>
2017-04-24 21:49:16 -0400

    add code to stop emitter after beginning game

--- js/states/stateTitle.js
+++ js/states/stateTitle.js
@@ -1,56 +1,56 @@
-//Bubble = function (manager) {
+Bubble = function (manager) {
 
-  //  Phaser.ParticleStorm.Particle.call(this, manager);
+   Phaser.ParticleStorm.Particle.call(this, manager);
 
-//};
+};
 
-//Bubble.prototype = Object.create(Phaser.ParticleStorm.Particle.prototype);
-//Bubble.prototype.constructor = Bubble;
+Bubble.prototype = Object.create(Phaser.ParticleStorm.Particle.prototype);
+Bubble.prototype.constructor = Bubble;
 
-//Bubble.prototype.onInherit = function(parent) {
+Bubble.prototype.onInherit = function(parent) {
 
     //  Child bubbles are killed if the parent is already at the top of the screen
-  //  if (parent.transform.y < 0)
-    //{
-      //  return false;
-    //}
+   if (parent.transform.y < 0)
+    {
+        return false;
+    }
 
     //  Child bubbles are smaller than their parent
-    //var ps = parent.transform.scale.x.value * (0.20 + Math.random() * 0.30);
+    var ps = parent.transform.scale.x.value * (0.20 + Math.random() * 0.30);
 
-    //this.transform.scale.x.value = ps;
-    //this.transform.scale.y.value = ps;
+    this.transform.scale.x.value = ps;
+    this.transform.scale.y.value = ps;
 
-    //  Kill children if they get too small
-    //if (this.transform.scale.x.value < 0.05)
-    //{
-      //  return false;
-    //}
+     // Kill children if they get too small
+    if (this.transform.scale.x.value < 0.05)
+     {
+        return false;
+      }
 
     //  Child bubbles start with random (low speed) velocities
-    //this.transform.velocity.x.value = (Math.random() - 0.5);
-    //this.transform.velocity.y.value = -Math.random();
+    this.transform.velocity.x.value = (Math.random() - 0.5);
+    this.transform.velocity.y.value = -Math.random();
 
-    //return true;
+    return true;
 
-//};
+};
 
-//Bubble.prototype.onUpdate = function() {
+Bubble.prototype.onUpdate = function() {
 
     //  Bigger bubbles rise faster
-  //  this.transform.velocity.y.value += -0.15 * this.transform.scale.y.value;
+    this.transform.velocity.y.value += -0.15 * this.transform.scale.y.value;
 
-    //if (this.transform.y < 0)
-    //{
-      //  this.kill();
-    //}
+    if (this.transform.y < 0)
+    {
+        this.kill();
+    }
 
-//};
+};
 
 
-//var manager = null;
-//var emitter = null;
-//var current = 0;
+var manager = null;
+var emitter = null;
+var current = 0;
 
 var StateTitle = {
 
@@ -79,56 +79,64 @@ var StateTitle = {
         gameMedia.updateMusic();
 
 
-       this.water=game.add.sprite(0,0,"water");
+        this.water=game.add.sprite(0,0,"water");
         this.water.sendToBack();
 
         this.title=game.add.sprite(game.world.centerX-200,game.world.centerY-175, "title");
 
         this.btnStart = gameButtons.addButton("start", -1, -1, this.startGame, this);
 
-  //      manager = this.game.plugins.add(Phaser.ParticleStorm);
+
+
+
+       manager = this.game.plugins.add(Phaser.ParticleStorm);
 
     //  This example illustrates:
 
-  //  var bubbles = {
-    //    image: 'bubble',
-     //   blendMode: 'ADD',
-      //  lifespan: 3000,
-     //   vx: { value: { min: -2, max: 2 }, control: [ { x: 0, y: 1 }, { x: 1, y: 0.1 } ] },
-      //  vy: { value: 0, control: [ { x: 0, y: 1 }, { x: 1, y: 0.1 } ] },
-      //  scale: { min: 0.25, max: 0.90 },
-     //   emit: {
-       //     name: 'bubbles',
-       //     value: 0, at: [ { time: 1, value: 5 } ],
-        //    inherit: { vx: true }
-     //   }
-   // };
+    var bubbles = {
+        image: 'bubble',
+        blendMode: 'ADD',
+        lifespan: 3000,
+        vx: { value: { min: -2, max: 2 }, control: [ { x: 0, y: 1 }, { x: 1, y: 0.1 } ] },
+        vy: { value: 0, control: [ { x: 0, y: 1 }, { x: 1, y: 0.1 } ] },
+        scale: { min: 0.25, max: 0.90 },
+        emit: {
+            name: 'bubbles',
+            value: 0, at: [ { time: 1, value: 5 } ],
+            inherit: { vx: true }
+       }
+    };
 
-  //  manager.addData('bubbles', bubbles);
+    manager.addData('bubbles', bubbles);
 
-  //  emitter = manager.createEmitter();
+    emitter = manager.createEmitter();
 
-  //  emitter.particleClass = Bubble;
+    emitter.particleClass = Bubble;
 
-  //  emitter.addToWorld();
+    emitter.addToWorld();
 
-  //  emitter.emit('bubbles', [-300, 800], 620, { repeat: -1, frequency: 10 });
+    emitter.emit('bubbles', [-300, 800], 620, { repeat: -1, frequency: 10 });
 
 
 
     }
 
+    , stopBubbles: function(){
+      emitter.enabled = false;
+    }
+
     , startGame: function () {
+        this.stopBubbles();
         game.state.start("StateMain");
     }
 
 
-   // , update: function () {
+    , update: function () {
          //  Make a current that blows left or right slowly changing strength and direction
-   // current = Math.max(Math.min(current + (Math.random() - .5) *.001, .05), -.05);
+    current = Math.max(Math.min(current + (Math.random() - .5) *.001, .05), -.05);
 
-   // emitter.force.x = current;
+    emitter.force.x = current;
 
-    //}
+    }
 
 }
